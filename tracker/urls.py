1e69fdcd80005450f66ea608ea825127e3c92a66
Eugene Hong <ehong270@gmail.com>
2022-09-12 18:19:42 -0400

    Feature 6 complete

--- tracker/urls.py
+++ tracker/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.urls import path, include, reverse_lazy
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
+    path(
+        "",
+        RedirectView.as_view(url=reverse_lazy("list_projects")),
+        name="home",
+    ),
 ]
