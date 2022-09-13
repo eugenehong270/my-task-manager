a5a6c1bcde26250aa93c22223a93a0c5a4026546
Eugene Hong <ehong270@gmail.com>
2022-09-12 20:35:30 -0400

    Feature 13 complete

--- projects/urls.py
+++ projects/urls.py
@@ -4,9 +4,11 @@ from django.contrib.auth import views as auth_views
 
 from projects.views import (
     ProjectListView,
+    ProjectDetailView,
 )
 
 
 urlpatterns = [
     path("", ProjectListView.as_view(), name="list_projects"),
+    path("<int:pk>/", ProjectDetailView.as_view(), name="show_project"),
 ]
